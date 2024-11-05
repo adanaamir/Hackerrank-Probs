7eee77a3ca23f556eac1b0ece711afc7ab452288
Adan Aamir <adanaamir2005@gmail.com>
2024-11-05 19:10:50 +0500

    .

--- FindAString.py
+++ FindAString.py
@@ -2,11 +2,12 @@ def count_substring(string, sub_string):
     start = 0
     count = 0
     while True:
-        start = string.find(sub_string, start)
-        if start == -1:
+        start = string.find(sub_string, start)   #using the find function to find a certain string in a string
+        if start == -1:   #since we're starting start from 0, if it equals -1 it means none is left
             break
-        count+=1
+        count+=1   # else incrementing count and start
         start+=1
+        
     return count
 
 
