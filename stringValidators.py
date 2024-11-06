b93da0d8ab6d54aa6032a2d7d997c6bbf4bfe7a7
Adan Aamir <adanaamir2005@gmail.com>
2024-11-06 17:57:03 +0500

    .

--- stringValidators.py
+++ stringValidators.py
@@ -1,24 +1,7 @@
 if __name__ == '__main__':
     s = input()
-
-    for i in s:
-        if i == s.isalnum():
-            print("True")
-        else:
-            print("False")
-        if i == s.isalpha():
-            print("True")
-        else:
-            print("False")
-        if i == s.isdigit():
-            print("True")
-        else:
-            print("False")
-        if i == s.islower():
-            print("True")
-        else:
-            print("False")
-        if i == s.isupper():
-            print("True")
-        else:
-            print("False")
+    print("True" if any(i.isalnum() for i in s) else "False" )
+    print("True" if any(i.isalpha() for i in s) else "False" )
+    print("True" if any(i.isdigit() for i in s) else "False" )
+    print("True" if any(i.islower() for i in s) else "False" )
+    print("True" if any(i.isupper() for i in s) else "False" )
--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("hi worrrrrld")
+def main(s):
+    print("True" if s.isalnum() else "False")
+    print("True" if s.isalpha() else "False")
+    print("True" if s.isdigit() else "False")
+    print("True" if s.islower() else "False")
+    print("True" if s.isupper() else "False")
+
+if __name__ == '__main__':
+    s = input()
+    main(s)
